a985544e9eb00e64cef62d33a922c059b6f98411
serega240805 <108699300+serega240805@users.noreply.github.com>
2022-07-23 15:13:42 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.testimonials__slider').slick({
         slidesToShow: 2,
-        dots:true,
-        adaptiveHeight:true,
+        dots: true,
+        adaptiveHeight: true,
         variableWidth: true,
-        slidesToScroll:2,
-        speed:1000,
+        slidesToScroll: 2,
+        speed: 1000,
     });
-    $('.faq__acc-link').on('click', function (e){
-        e.preventDefault()
-        $(this).toggleClass('faq__acc-link--active')
-        $(this).children('.faq__acc-text').slideToggle()
-    })
+    var accordeonButtons = document.getElementsByClassName("accordeon__button");
+
+    for (var i = 0; i < accordeonButtons.length; i++) {
+        var accordeonButton = accordeonButtons[i];
+
+        accordeonButton.addEventListener("click", toggleItems, false);
+    }
+
+    function toggleItems() {
+
+        var itemClass = this.className;
+
+        for (var i = 0; i < accordeonButtons.length; i++) {
+            accordeonButtons[i].className = "accordeon__button closed";
+        }
+
+        var pannels = document.getElementsByClassName("accordeon__panel");
+        for (var z = 0; z < pannels.length; z++) {
+            pannels[z].style.maxHeight = 0;
+        }
+
+        if (itemClass == "accordeon__button closed") {
+            this.className = "accordeon__button active";
+            var panel = this.nextElementSibling;
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+
+    }
 })
