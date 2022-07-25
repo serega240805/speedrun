9b636e19f028ea8afbcb52b994f236f02d310bf8
serega240805 <108699300+serega240805@users.noreply.github.com>
2022-07-26 00:41:17 +0300

    new

--- js/main.js
+++ js/main.js
@@ -36,3 +36,8 @@ $(document).ready(function () {
 
     }
 })
+var toggleButton = document.querySelector('.toggle-menu');
+var navBar = document.querySelector('.nav-bar');
+toggleButton.addEventListener('click', function () {
+	navBar.classList.toggle('toggle');
+});
