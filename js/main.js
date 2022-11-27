19c885c14f0229ae71109a4f8022ef1a102abbfb
serega240805 <108699300+serega240805@users.noreply.github.com>
2022-11-27 22:42:53 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,43 +1,45 @@
-$(document).ready(function () {
-    $('.testimonials__slider').slick({
-        slidesToShow: 2,
-        dots: true,
-        adaptiveHeight: true,
-        variableWidth: true,
-        slidesToScroll: 2,
-        speed: 1000,
+$(document).ready(function() {
+
+    function close_accordion_section() {
+        $('.accordion .title').removeClass('active');
+        $('.accordion .section-content').slideUp(300).removeClass('open');
+    }
+
+    $('.title').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
+        }
+
+        e.preventDefault();
     });
-    var accordeonButtons = document.getElementsByClassName("accordeon__button");
 
-    for (var i = 0; i < accordeonButtons.length; i++) {
-        var accordeonButton = accordeonButtons[i];
-
-        accordeonButton.addEventListener("click", toggleItems, false);
-    }
-
-    function toggleItems() {
-
-        var itemClass = this.className;
-
-        for (var i = 0; i < accordeonButtons.length; i++) {
-            accordeonButtons[i].className = "accordeon__button closed";
-        }
-
-        var pannels = document.getElementsByClassName("accordeon__panel");
-        for (var z = 0; z < pannels.length; z++) {
-            pannels[z].style.maxHeight = 0;
-        }
-
-        if (itemClass == "accordeon__button closed") {
-            this.className = "accordeon__button active";
-            var panel = this.nextElementSibling;
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-
-    }
-})
-var toggleButton = document.querySelector('.toggle-menu');
-var navBar = document.querySelector('.nav-bar');
-toggleButton.addEventListener('click', function () {
-	navBar.classList.toggle('toggle');
+});//ready 
+new Swiper('.reviews__slider', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 'auto',
+    spaceBetween: 260,
+    slidesPerGroup: 1,
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    autoplay: {
+        delay: 5000,
+        speed: 100,
+    },
 });
